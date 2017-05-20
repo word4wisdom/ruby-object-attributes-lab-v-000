df55ed1755eac141d05e168f428acc835020fc70
Joan' Araujo <wisdombthegame@gmail.com>
2017-05-20 22:07:15 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+def name=name
+  @name = "Fido"
+end
+
+def name
+  @name
+  end
+
+def breed=breed
+  @breed = "Beagle"
+  end
+
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+def name=name
+@name = "Beyonce"
+end
+
+def name
+  @name
+  end
+
+def job=job
+  @job = "Singer"
+end
+
+def job
+  @job
+end
+end
+
+ 
